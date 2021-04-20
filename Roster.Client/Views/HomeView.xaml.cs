26c56e9222eb5387fcc5d178bdffd5ab791fe1e3
Maurilio Filho <mauriliofilho22@gmail.com>
2021-04-20 01:16:40 -0300

    work with data-binding

--- Roster.Client/Views/HomeView.xaml.cs
+++ Roster.Client/Views/HomeView.xaml.cs
@@ -1,13 +1,21 @@
-﻿using Xamarin.Forms;
+﻿using System.Reflection;
+using Xamarin.Forms;
 
 namespace Roster.Client.Views
 {
     [System.ComponentModel.DesignTimeVisible(false)]
     public partial class HomeView : ContentPage
     {
+        
         public HomeView()
         {
             InitializeComponent();
+            BindingContext = new Header();
         }
+    }
+
+    public class Header
+    {
+        public string Title { get => "Roster App"; }
     }
 }
